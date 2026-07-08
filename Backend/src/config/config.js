9eb27217fb7e046c586ad6ff647035758c5983e9
Sachin Kumar <sachin070502@gmail.com>
2026-07-08 23:44:47 +0530

    feat(product): integrate ImageKit for file uploads and add multer middleware

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -17,6 +17,10 @@ if(!process.env.GOOGLE_CLIENT_SECRET){
     throw new Error ("GOOGLE_CLIENT_SECRET is not defined in the environment variables");
 }
 
+if(!process.env.IMAGEKIT_PRIVATE_KEY){
+    throw new Error ("IMAGEKIT_PRIVATE_KEY is not defined in the environment variables");
+}
+
 export const config = {
    MONGO_URI: process.env.MONGO_URI,
    JWT_SECRET: process.env.JWT_SECRET,
@@ -24,5 +28,6 @@ export const config = {
    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-   GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/auth/google/callback"
+   GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/auth/google/callback",
+   IMAGEKIT_PRIVATE_KEY: process.env.IMAGEKIT_PRIVATE_KEY
 }
--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import { authenticateSeller } from '../middlewares/auth.middleware'
+import { createProduct } from '../controllers/product.controller.js';
+import multer from 'multer';
 
-
+const upload = multer(
+    {
+        storage: multer.memoryStorage(),
+        limits: {
+            fileSize: 5 * 1024 * 1024 // 5MB
+        }
+    }
+);
 const router = express.Router();
 
-router.post("/", authenticateSeller, createProduct)
+router.post("/", authenticateSeller, upload.array('images', 7), createProduct)
 
 
 
