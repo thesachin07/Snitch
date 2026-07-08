fa7427449eab4e7d14e3e7f4491a93484cf26b87
Sachin Kumar <sachin070502@gmail.com>
2026-07-08 20:18:53 +0530

    feat(auth): implement seller authentication middleware for product routes

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,3 +1,29 @@
 import jwt from "jsonwebtoken";
 import { config } from "../config/config.js";
 
+export const authenticateSeller = async (req, res, next) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({ message: "unauthorized" });
+
+    }
+
+    try {   
+        const decoded = jwt.verify(token, config.JWT_SECRET);
+
+        const user = await useModel.findById(decoded.id);
+        if(!user){
+            return res.status(401).json({ message: "Unauthorized" });
+
+        }
+        if (user.role !== "seller") {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+        req.user = user
+        next()
+    }
+        catch (err) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+    }
--- Backend/src/routes/product.routes.js
+++ Backend/src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import { authenticateSeller } from '../middlewares/auth.middleware'
+
 
 const router = express.Router();
 
-router.post("/"), (req, res) => {
-    res.status(200).json({message: "Product route is working"});
-}
+router.post("/", authenticateSeller, createProduct)
+
 
 
 
